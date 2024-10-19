9bfaadc3ad85b0c8476e6d538266f57c76f33bfd
elvnish <130566277+elvnish@users.noreply.github.com>
2024-10-19 15:26:05 +0200

    ADDED Buttons + Function to MainMenu
    ya button time

--- WitchRoad/Assets/Scripts/UI/Menu.cs
+++ WitchRoad/Assets/Scripts/UI/Menu.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Play()
     {
-        
+        SceneManager.LoadScene("Train Scene"); // add to build order
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Quit()
     {
-        
+        Application.Quit();
     }
 }
