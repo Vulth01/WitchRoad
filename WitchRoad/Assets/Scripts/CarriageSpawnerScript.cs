4a0b4bd3a0addb997294cca6b456dcc45cf17c33
Vulth01 <damienharwood14@gmail.com>
2024-10-19 14:17:35 +0200

    Update: Player Movement | Add: Carriage Spawning

---
+++ WitchRoad/Assets/Scripts/CarriageSpawnerScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarriageSpawnerScript : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private float spawnTimer;
+    [SerializeField] private float sTimer;
+    [SerializeField] private float xOffset;
+    [SerializeField] private float yOffset;
+    [SerializeField] private float zOffset;
+    [SerializeField] private int instanceCount;
+    [SerializeField] private GameObject prefab1;
+    [SerializeField] private GameObject prefab2;
+
+    private void FixedUpdate()
+    {
+        doTimer();      // For testing carriage spawning
+    }
+
+    void doTimer()
+    {
+
+        sTimer = sTimer - (1 * Time.deltaTime);
+        if (sTimer <= 0)
+        {
+            Debug.Log(sTimer);
+            DoSpawn();
+            sTimer = spawnTimer;
+        }
+    }
+
+
+    private void DoSpawn()
+    {
+        instanceCount++;
+        var pos = gameObject.transform.position;
+        var spawnPos = new Vector3(pos.x + (xOffset * instanceCount), pos.y + (yOffset * instanceCount), pos.z + (zOffset * instanceCount));
+        Destroy(prefab1);
+        prefab1 = prefab2;
+        prefab2 = Instantiate(prefab, spawnPos, Quaternion.identity, gameObject.transform);
+    }
+
+}
