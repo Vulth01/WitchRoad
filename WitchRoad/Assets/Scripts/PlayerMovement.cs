6b50e8aec40fbfbf918a21bd8662d58d05e1bf17
Vulth01 <damienharwood14@gmail.com>
2024-10-19 12:59:55 +0200

    Add: PlayerMovement, Player

--- WitchRoad/Assets/Scripts/PlayerMovement.cs
+++ WitchRoad/Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //UnityInputSystem.Apply(); Yes.
+    private GameObject playerObj;
+    private Rigidbody playerRb;
+    private PlayerInput input;
+    private InputAction move;
+    private InputAction jump;
+    private bool isGrounded;
+    private float jumpHeight = 15;
+
+    private void Awake()
+    {
+        playerObj = this.gameObject;
+        input = GetComponent<PlayerInput>();
+        move = input.actions["move"];
+        jump = input.actions["jump"];
+    }
+
+    private void Update()
+    {
+        DoJump();
+    }
+
+    private void DoJump()
+    {
+        Vector2 moveVector = move.ReadValue<Vector2>();
+        if (jump.IsPressed() && isGrounded)
+        {
+            playerRb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+        }
+    }
+
+
+
 }
